e042f1eaf9413aa01de5c0c2fc7e8586e123f0ed
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2025-05-03 18:29:12 +0300

    Setup config and eureka servers

--- config/src/main/java/edu/microchat/config/ConfigServer.java
+++ config/src/main/java/edu/microchat/config/ConfigServer.java
@@ -2,12 +2,12 @@ package edu.microchat.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class ConfigApplication {
-
+public class ConfigServer {
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
+		SpringApplication.run(ConfigServer.class, args);
 	}
-
 }
--- eureka/src/main/java/edu/microchat/eureka/EurekaServer.java
+++ eureka/src/main/java/edu/microchat/eureka/EurekaServer.java
@@ -2,12 +2,14 @@ package edu.microchat.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplication {
+public class EurekaServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(EurekaServer.class, args);
 	}
 
 }
