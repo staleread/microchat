af7f8ed0adb06be528d59aac8a940e3809422878
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2025-05-08 23:23:35 +0300

    Add validation exception handlers

---
+++ message/src/main/java/edu/microchat/message/common/ValidationExceptionHandler.java
@@ -0,0 +1,33 @@
+package edu.microchat.message.common;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+
+  @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+  public Map<String, String> handleValidationException(MethodArgumentNotValidException exception) {
+    var errorsMap = new HashMap<String, String>();
+
+    exception
+        .getBindingResult()
+        .getAllErrors()
+        .forEach(
+            error -> {
+              String fieldName = ((FieldError) error).getField();
+              String errorMessage = error.getDefaultMessage();
+
+              errorsMap.put(fieldName, errorMessage);
+            });
+
+    return errorsMap;
+  }
+}
