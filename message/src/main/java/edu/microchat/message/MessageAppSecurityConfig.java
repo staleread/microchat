912de3b2573d6c2a6a8ff286d2b46ffbfb950c94
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2025-10-11 12:27:10 +0300

    Disable CSRF and don't allow request without authentification

---
+++ message/src/main/java/edu/microchat/message/MessageAppSecurityConfig.java
@@ -0,0 +1,21 @@
+package edu.microchat.message;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class MessageAppSecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        .httpBasic(Customizer.withDefaults())
+        .csrf(csrf -> csrf.disable());
+
+    return http.build();
+  }
+}
